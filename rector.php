4667f6d4e82e9b4b9a2c95b4a220269fcb9aa9fc
Thomas LEDUC <thomaslmoi15@hotmail.fr>
2025-09-23 17:24:58 +0200

    Update project to Symfony 5.3

--- rector.php
+++ rector.php
@@ -19,7 +19,7 @@ return RectorConfig::configure()
     ->withPhp72Sets()
     ->withAttributesSets(true)
     ->withSets([
-        SymfonySetList::SYMFONY_52,
+        SymfonySetList::SYMFONY_53,
         SetList::DEAD_CODE,
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE
